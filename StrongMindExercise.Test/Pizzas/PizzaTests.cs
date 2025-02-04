63be43096f41caf523d6f8e2230c85161667c1ae
dwathen <67804842+dwathen@users.noreply.github.com>
2025-02-04 09:27:29 -0700

    Added expected test cases for this project for both Pizza object and Topping object - automatically asserts true for now so that I can build out test cases as I build in features.

---
+++ StrongMindExercise.Test/Pizzas/PizzaTests.cs
@@ -0,0 +1,135 @@
+﻿namespace StrongMindExercise.Test.Pizzas;
+public class PizzaTests
+{
+    [Fact]
+    public void GetAllPizzas_ShouldReturnAllToppings()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void CreatePizza_ShouldCreateTopping_WhenNotDuplicate()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void CreatePizza_ShouldReturnError_WhenDuplicate()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+        [Fact]
+    public void CreatePizza_ShouldReturnError_WhenZeroChildren()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void CreatePizza_ShouldReturnError_WhenDuplicateChildren()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void UpdatePizza_ShouldUpdateTopping_WhenValid()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void UpdatePizza_ShouldReturnError_WhenUpdateToDuplicate()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void UpdatePizza_ShouldReturnError_WhenZeroChildren()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void UpdatePizza_ShouldReturnError_WhenDuplicateChildren()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void UpdatePizza_ShouldReturnError_WhenNotExists()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void DeletePizza_ShouldDeleteTopping_WhenExists()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void DeletePizza_ShouldReturnError_WhenNotExists()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ StrongMindExercise.Test/Toppings/ToppingTests.cs
@@ -0,0 +1,92 @@
+﻿namespace StrongMindExercise.Test.Toppings;
+public class ToppingTests
+{
+
+    [Fact]
+    public void GetAllToppings_ShouldReturnAllToppings()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void CreateTopping_ShouldCreateTopping_WhenNotDuplicate()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void CreateTopping_ShouldReturnError_WhenDuplicate()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void UpdateToppings_ShouldUpdateTopping_WhenValid()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void UpdateToppings_ShouldReturnError_WhenUpdateToDuplicate()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void UpdateToppings_ShouldReturnError_WhenNotExists()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void DeleteTopping_ShouldDeleteTopping_WhenExists()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void DeleteTopping_ShouldReturnError_WhenNotExists()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        Assert.True(true);
+    }
+}
